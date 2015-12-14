3cdae4bf174490d4f51ae62fd4adfc334b81bb1b
JamStop <jimmyyue1@gmail.com>
2015-12-14 15:31:49 -0800

    pod install and database setup

--- KhanAcademyBadges/Models/Database.swift
+++ KhanAcademyBadges/Models/Database.swift
@@ -7,3 +7,96 @@
 //
 
 import Foundation
+import JSONHelper
+import RxSwift
+import Alamofire
+
+class Database {
+    
+    typealias JSON = [String: AnyObject]
+    typealias CompletionHandler = (response: JSON? , error: DatabaseError?) -> Void
+    
+    private let baseURL = "https://www.khanacademy.org/api/v1/"
+    private let unexpectedResponse = "Unexpected response."
+    
+    enum DatabaseError: ErrorType {
+        case NoResponse
+        case ErrorParsingJSON
+        case ResponseError(message: String?, error: String?)
+        case UnexpectedResponseError(message: String?, statusCode: Int?)
+        case UnexpectedError(message: String)
+    }
+
+    
+    func rx_getBadgeCategories() -> Observable<JSON?> {
+
+        return create { observer in
+            
+            self.get("badges/categories") {
+                (resp, err) -> Void in
+                
+                if let error = err {
+                    observer.onError(error)
+                } else {
+                    observer.onNext(resp)
+                    observer.onCompleted()
+                }
+            }
+            
+            return NopDisposable.instance
+        }
+        
+    }
+    
+    private func get(endPoint: String, handler: CompletionHandler?) {
+        
+        request(.GET, endpoint: endPoint, handler: handler)
+        
+    }
+    
+    private func post(endPoint: String, handler: CompletionHandler?) {
+        
+        request(.POST, endpoint: endPoint, handler: handler)
+        
+    }
+    
+    private func request(method: Alamofire.Method, endpoint: String, handler: CompletionHandler?) {
+        
+        let encoding: Alamofire.ParameterEncoding = method == Alamofire.Method.GET ? .URL : .JSON
+        
+        Alamofire.request(method, baseURL + endpoint, encoding: encoding, headers: nil)
+            .responseJSON(options: .AllowFragments) { (Response) -> Void in
+                
+                guard let response = Response.response else {
+                    handler?(response: nil, error: .NoResponse)
+                    return
+                }
+                
+                if (response.statusCode == 200) {
+                    
+                    guard let responseJSON = Response.result.value as? JSON else {
+                        handler?(response: nil, error: .ErrorParsingJSON)
+                        return
+                    }
+                    
+                    if let error = responseJSON["error"] {
+                        // error fetching data
+                        let message = responseJSON["message"] as? String
+                        handler?(response: nil, error: .ResponseError(message: message, error: error as? String))
+                    } else {
+                        // no errors
+                        handler?(response: responseJSON, error: nil)
+                    }
+                    
+                } else {
+                    
+                    handler?(response: nil, error: .UnexpectedResponseError(message: "Unexpected error", statusCode: response.statusCode))
+                    
+                }
+                
+        }
+        
+    }
+
+    
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+
+use_frameworks!
+
+pod 'Alamofire'
+pod 'SDWebImage'
+pod 'JSONHelper'
+pod 'RxSwift', '~> 2.0.0-beta'
+pod 'RxCocoa', '~> 2.0.0-beta'
+pod 'RxBlocking', '~> 2.0.0-beta'
+
