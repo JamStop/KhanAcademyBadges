d5a132f64908f7c223962d6501aad1853e2e8115
JamStop <jimmyyue1@gmail.com>
2015-12-14 14:54:16 -0800

    Init

---
+++ KhanAcademyBadges/Models/Database.swift
@@ -0,0 +1,9 @@
+//
+//  Database.swift
+//  KhanAcademyBadges
+//
+//  Created by Jimmy Yue on 12/14/15.
+//  Copyright © 2015 Augmented Humanity. All rights reserved.
+//
+
+import Foundation
---
+++ KhanAcademyBadgesTests/KhanAcademyBadgesTests.swift
@@ -0,0 +1,36 @@
+//
+//  KhanAcademyBadgesTests.swift
+//  KhanAcademyBadgesTests
+//
+//  Created by Jimmy Yue on 12/14/15.
+//  Copyright © 2015 Augmented Humanity. All rights reserved.
+//
+
+import XCTest
+@testable import KhanAcademyBadges
+
+class KhanAcademyBadgesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
