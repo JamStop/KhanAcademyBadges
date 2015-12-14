d035c5daea9988f73f97596dd401db3402fe4632
JamStop <jimmyyue1@gmail.com>
2015-12-14 15:38:21 -0800

    switch to tableview

---
+++ KhanAcademyBadges/View Controllers/BadgesTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  BadgesTableViewController.swift
+//  KhanAcademyBadges
+//
+//  Created by Jimmy Yue on 12/14/15.
+//  Copyright © 2015 Augmented Humanity. All rights reserved.
+//
+
+import UIKit
+
+class BadgesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+
+        return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+//    override func table
+
+}
